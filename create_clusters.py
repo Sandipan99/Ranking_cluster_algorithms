ff8a3a0c2da10722aa4b035a92133e0f2a1c0af4
Sandipan99 <sikdarsandipan99@gmail.com>
2020-05-10 16:15:56 +0200

    commit

--- create_clusters.py
+++ create_clusters.py
@@ -1,4 +1,4 @@
-from sklearn.cluster import SpectralClustering, Birch
+from sklearn.cluster import SpectralClustering, Birch, AgglomerativeClustering, AffinityPropagation, KMeans
 import numpy as np
 
 
@@ -23,21 +23,59 @@ def saveOutput(fname, clustering): # saves output with each line as the label an
 
 
 
-data = np.loadtxt('Data-GT/Whitewine/rank_features',delimiter=',')
-data = np.transpose(data)
-print(data.shape)
+data = np.loadtxt('Data-GT/US_consensus/dataset',delimiter=',')
 
-'''
 print('fitting clusters')
-clustering_spec = SpectralClustering(n_clusters=7, assign_labels="discretize").fit(data)
+clustering_spec = SpectralClustering(n_clusters=10, assign_labels="discretize").fit(data)
 print('fitting finished')
 
-saveOutputlinewise('Data-GT/Whitewine/spectral_cluster_rank', clustering_spec)
-saveOutput('Data-GT/plos-one/Whitewine/spectral_cluster_rank', clustering_spec)
-'''
+saveOutputlinewise('Data-GT/US_consensus/spectral_cluster', clustering_spec)
+saveOutput('Data-GT/plos-one/US_consensus/spectral_cluster', clustering_spec)
+
 print('fitting clusters')
-clustering_b = Birch(n_clusters=7).fit(data)
+clustering_b = Birch(n_clusters=10).fit(data)
 print('fitting finished')
 
-saveOutputlinewise('Data-GT/Whitewine/birch_cluster_rank', clustering_b)
-saveOutput('Data-GT/plos-one/Whitewine/birch_cluster_rank', clustering_b)
+saveOutputlinewise('Data-GT/US_consensus/birch_cluster', clustering_b)
+saveOutput('Data-GT/plos-one/US_consensus/birch_cluster', clustering_b)
+
+print('fitting clusters')
+clustering_comp_eu = AgglomerativeClustering(n_clusters=10, affinity='euclidean',linkage='complete').fit(data)
+clustering_avg_eu = AgglomerativeClustering(n_clusters=10, affinity='euclidean',linkage='average').fit(data)
+clustering_sing_eu = AgglomerativeClustering(n_clusters=10, affinity='euclidean',linkage='single').fit(data)
+print('fitting finished')
+
+saveOutputlinewise('Data-GT/US_consensus/complete_euclidean_cluster', clustering_comp_eu)
+saveOutput('Data-GT/plos-one/US_consensus/complete_euclidean_cluster', clustering_comp_eu)
+saveOutputlinewise('Data-GT/US_consensus/average_euclidean_cluster', clustering_avg_eu)
+saveOutput('Data-GT/plos-one/US_consensus/average_euclidean_cluster', clustering_avg_eu)
+saveOutputlinewise('Data-GT/US_consensus/single_euclidean_cluster', clustering_sing_eu)
+saveOutput('Data-GT/plos-one/US_consensus/single_euclidean_cluster', clustering_sing_eu)
+
+print('fitting clusters')
+clustering_comp_cb = AgglomerativeClustering(n_clusters=10, affinity='cityblock',linkage='complete').fit(data)
+clustering_avg_cb = AgglomerativeClustering(n_clusters=10, affinity='cityblock',linkage='average').fit(data)
+clustering_sing_cb = AgglomerativeClustering(n_clusters=10, affinity='cityblock',linkage='single').fit(data)
+print('fitting finished')
+
+saveOutputlinewise('Data-GT/US_consensus/complete_cityblock_cluster', clustering_comp_cb)
+saveOutput('Data-GT/plos-one/US_consensus/complete_cityblock_cluster', clustering_comp_cb)
+saveOutputlinewise('Data-GT/US_consensus/average_cityblock_cluster', clustering_avg_cb)
+saveOutput('Data-GT/plos-one/US_consensus/average_cityblock_cluster', clustering_avg_cb)
+saveOutputlinewise('Data-GT/US_consensus/single_cityblock_cluster', clustering_sing_cb)
+saveOutput('Data-GT/plos-one/US_consensus/single_cityblock_cluster', clustering_sing_cb)
+
+print('fitting clusters')
+clustering_ap = AffinityPropagation().fit(data)
+print('fitting finished')
+
+saveOutputlinewise('Data-GT/US_consensus/ap_cluster', clustering_ap)
+saveOutput('Data-GT/plos-one/US_consensus/ap_cluster', clustering_ap)
+
+print('fitting clusters')
+clustering_km = KMeans(n_clusters=10).fit(data)
+print('fitting finished')
+
+saveOutputlinewise('Data-GT/US_consensus/kmeans_cluster', clustering_km)
+saveOutput('Data-GT/plos-one/US_consensus/kmeans_cluster', clustering_km)
+
